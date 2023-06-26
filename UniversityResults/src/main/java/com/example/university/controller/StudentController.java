9a7a0f7dec3dac0557107375174d0ad39e46e070
ZiaUlRahman <106248731+ZiaRahmann@users.noreply.github.com>
2023-06-26 12:53:42 +0530

    Update StudentController.java

--- UniversityResults/src/main/java/com/example/university/controller/StudentController.java
+++ UniversityResults/src/main/java/com/example/university/controller/StudentController.java
@@ -8,10 +8,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/student")
 public class StudentController {
 
+	@Autowired
+	StudentRepository studentrepo;
+	
+	@Autowired
+	StudentServiceImpl studentservice;
+	
+	
 	@PostMapping(value="/add")
-	public String addUser() {
-		return "User Created..!!!";
+	public Student createUser(Student studentId) {
+		
+		Student stu1 = studentservice.createUser(Student);
+		return studentId;
 	}
+	
+	@GetMapping(value ="/getuserbyid")
+	public Student getUserById(Student studentId) {
+		
+		return studentId;
+	}
+	
+	@GetMapping(value ="/viewall")
+	public Student viewAllUsers(Student student)  {
+		
+		return student;
+	}
+	
+	@GetMapping(value ="/getuserbyname")
+	public Student getUserByName(Student studentName) {
+		
+		return studentName;
+	}
+	
 	
 	
 	
